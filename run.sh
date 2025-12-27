1d019913aa34848c1e9f794247ecf5b9c13002ef
lakshyalayman <lakshya1.web@gmail.com>
2025-12-27 12:36:15 +0530

    change of plans

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-CFLAGS="-Wall -Wextra `pkg-config --cflags raylib`"
-LIBS="`pkg-config --libs raylib` -lglfw -lm -ldl -lpthread"
-
-clang $CFLAGS -o plot main.c $LIBS
-./plot
