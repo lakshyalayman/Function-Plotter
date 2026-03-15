3ee6d2b4c5c412062eec7800b22d687139e4c422
lakshyalayman <lakshya1.web@gmail.com>
2026-03-15 13:30:18 +0530

    VLA

--- main.c
+++ main.c
@@ -1,50 +1,70 @@
+#include <raylib.h>
 #define PLUG_IMPLEMENTATION
 #include "plug.h"
 
-int counta = 0;
-
-int main(int argc,char *argv[]) {
-  if(argc < 2){
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
     printf("BAD USAGE: ./run.sh \"function\"");
     return -1;
   }
-  counta = argc;
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-  InitWindow(WIDTH,HEIGHT, "Function Plotter");
+  InitWindow(WIDTH, HEIGHT, "Function Plotter");
   Camera2D camera = {0};
   camera.offset = (Vector2){400, 225};
   camera.target = (Vector2){0, 0};
   camera.zoom = 1.0f;
   SetTargetFPS(60);
 
+  bool marker = false;
   while (!WindowShouldClose()) {
-    camera.offset = (Vector2){ GetScreenWidth()/2.0f, GetScreenHeight()/2.0f };
+    camera.offset =
+        (Vector2){GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
     camera.zoom += GetMouseWheelMove() * 0.2f;
-    if (camera.zoom < 0.1f) camera.zoom = 0.1f;
-    if(camera.zoom > 3.0f) camera.zoom = 3.0f;
-    if(IsKeyPressed(KEY_O)){
+    if (camera.zoom < 0.1f)
+      camera.zoom = 0.1f;
+    if (camera.zoom > 3.0f)
+      camera.zoom = 3.0f;
+    if (IsKeyPressed(KEY_O)) {
       camera.target.x = 0;
       camera.target.y = 0;
+      camera.zoom = 1.0f;
     }
-    if(IsKeyPressed(KEY_Q))break;
-    if (IsKeyDown(KEY_LEFT_SHIFT)&&IsKeyDown(KEY_RIGHT)) camera.target.x += 10;
-    else if (IsKeyDown(KEY_RIGHT)) camera.target.x += 5;
-    if (IsKeyDown(KEY_LEFT_SHIFT)&&IsKeyDown(KEY_LEFT)) camera.target.x -= 10;
-    else if (IsKeyDown(KEY_LEFT))  camera.target.x -= 5;
-    if (IsKeyDown(KEY_LEFT_SHIFT)&&IsKeyDown(KEY_UP)) camera.target.y -= 10;
-    else if (IsKeyDown(KEY_UP))    camera.target.y -= 5;
-    if (IsKeyDown(KEY_LEFT_SHIFT)&&IsKeyDown(KEY_DOWN)) camera.target.y += 10;
-    else if (IsKeyDown(KEY_DOWN))  camera.target.y += 5;
+    if (IsKeyPressed(KEY_Q))
+      break;
+    if (IsKeyDown(KEY_LEFT_SHIFT) && IsKeyDown(KEY_RIGHT))
+      camera.target.x += 10;
+    else if (IsKeyDown(KEY_RIGHT))
+      camera.target.x += 5;
+    if (IsKeyDown(KEY_LEFT_SHIFT) && IsKeyDown(KEY_LEFT))
+      camera.target.x -= 10;
+    else if (IsKeyDown(KEY_LEFT))
+      camera.target.x -= 5;
+    if (IsKeyDown(KEY_LEFT_SHIFT) && IsKeyDown(KEY_UP))
+      camera.target.y -= 10;
+    else if (IsKeyDown(KEY_UP))
+      camera.target.y -= 5;
+    if (IsKeyDown(KEY_LEFT_SHIFT) && IsKeyDown(KEY_DOWN))
+      camera.target.y += 10;
+    else if (IsKeyDown(KEY_DOWN))
+      camera.target.y += 5;
     BeginDrawing();
     ClearBackground(BLACK);
     BeginMode2D(camera);
-      ScreenGrid(camera,SPACING);
-      for(int i = 1;i<argc;i++){
-        DrawMathFunction(camera,argv[i],i);
+    ScreenGrid(camera, SPACING);
+    for (int i = 1; i < argc; i++) {
+      if (!DrawMathFunction(camera, argv[i], i, argc)) {
+        // marker = true;
+        break;
       }
-      directions(camera);
+    }
+    directions(camera);
     EndMode2D();
-      DrawText(TextFormat("Target: X:%.0f Y:%.0f", camera.target.x, -camera.target.y), 10, 10, 20, BLUE);
+    if (marker) {
+      DrawText("parse error", 30, 40, 20, RED);
+    }
+    DrawText(
+        TextFormat("Target: X:%.0f Y:%.0f", camera.target.x, -camera.target.y),
+        10, 10, 20, BLUE);
     EndDrawing();
   }
   CloseWindow();
--- plug.h
+++ plug.h
@@ -1,81 +1,86 @@
 #ifndef PLUG_H
 #define PLUG_H
 
-#include <raylib.h>
-#include <math.h>
-#include <complex.h>
 #include "tinyexpr.h"
+#include <complex.h>
+#include <math.h>
+#include <raylib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define WIDTH 800
 #define HEIGHT 450
 #define SPACING 40.0f
-#define N (3*WIDTH/SPACING + 1)
-
-extern int counta;
+#define N (3 * WIDTH / SPACING + 1)
 
 void directions(Camera2D camera);
-void DrawMathFunction(Camera2D camera,char *stringFunction,int curr);
-void ScreenGrid(Camera2D camera,float spacing);
+bool DrawMathFunction(Camera2D camera, char *stringFunction, int curr,
+                      int counta);
+void ScreenGrid(Camera2D camera, float spacing);
 
 #endif // !PLUG_H
-// #define PLUG_IMPLEMENTATION
+#define PLUG_IMPLEMENTATION
 #ifdef PLUG_IMPLEMENTATION
 
-void DrawMathFunction(Camera2D camera,char *stringFunction,int curr) {
+bool DrawMathFunction(Camera2D camera, char *stringFunction, int curr,
+                      int counta) {
   int width = GetScreenWidth();
   double x_var = 0;
-  Vector2 points[width];
-  Vector2 bottomPoints[width];
+  Vector2 *points = (Vector2 *)malloc(width * sizeof(Vector2));
+  if (!points)
+    return false;
+  // Vector2 bottomPoints[width];
+  // int count = 0;
+  // bool marker = false;
   Color color;
-  int count = 0;
-  bool marker = false;
-  te_variable vars[] = {
-    {"x",&x_var}
-  };
+  te_variable vars[] = {{"x", &x_var}};
   int err;
-  te_expr *userFunction = te_compile(stringFunction,vars,1,&err);
-  if(userFunction){
+  te_expr *userFunction = te_compile(stringFunction, vars, 1, &err);
+  if (userFunction) {
     for (int i = 0; i < width; i++) {
-      Vector2 worldPos = GetScreenToWorld2D((Vector2){ (float)i, 0 }, camera);
-      x_var = worldPos.x / SPACING; 
-      float y = te_eval(userFunction)*SPACING;
-      points[i] = (Vector2){worldPos.x, -y }; 
+      Vector2 worldPos = GetScreenToWorld2D((Vector2){(float)i, 0}, camera);
+      x_var = worldPos.x / SPACING;
+      float y = te_eval(userFunction) * SPACING;
+      points[i] = (Vector2){worldPos.x, -y};
     }
     te_free(userFunction);
-    color = ColorFromHSV(360*((float)curr/counta),1.00,1.00);
-    DrawLineStrip(points, width,color);
-  }else{
-    int width = GetScreenWidth();
-    int height = GetScreenHeight();
-    DrawText("code broke :(",(float)width/2,(float)height/2,30,RED);
+    color = ColorFromHSV(360 * ((float)curr / counta), 1.00, 1.00);
+    DrawLineStrip(points, width, color);
+  } else {
+    // DrawText("code broke :(", (int)camera.target.x, (int)camera.target.y, 30,
+    // RED);
+    return false;
   }
+  free(points);
+  return true;
 }
 
-void directions(Camera2D camera){
-  DrawCircle(camera.target.x,camera.target.y,2,RED);
+void directions(Camera2D camera) {
+  DrawCircle(camera.target.x, camera.target.y, 2, RED);
 }
 
-void ScreenGrid(Camera2D camera,float spacing){
+void ScreenGrid(Camera2D camera, float spacing) {
   float width = GetScreenWidth();
   float height = GetScreenHeight();
 
-  Vector2 topLeft = GetScreenToWorld2D((Vector2){0,0}, camera);
-  Vector2 bottomRight = GetScreenToWorld2D((Vector2){width,height}, camera);
+  Vector2 topLeft = GetScreenToWorld2D((Vector2){0, 0}, camera);
+  Vector2 bottomRight = GetScreenToWorld2D((Vector2){width, height}, camera);
 
   float startX = floorf(topLeft.x / spacing) * spacing;
   float startY = floorf(topLeft.y / spacing) * spacing;
 
   for (float x = startX; x <= bottomRight.x; x += spacing) {
-    float thickness = (fabsf(x) < 0.1f) ? 2.0f : 1.0f; 
+    float thickness = (fabsf(x) < 0.1f) ? 2.0f : 1.0f;
     Color col = (fabsf(x) < 0.1f) ? WHITE : DARKGRAY;
-    DrawLineEx((Vector2){ x, topLeft.y }, (Vector2){ x, bottomRight.y }, thickness, col);
+    DrawLineEx((Vector2){x, topLeft.y}, (Vector2){x, bottomRight.y}, thickness,
+               col);
   }
 
   for (float y = startY; y <= bottomRight.y; y += spacing) {
     float thickness = (fabsf(y) < 0.1f) ? 3.0f : 1.0f;
     Color col = (fabsf(y) < 0.1f) ? WHITE : DARKGRAY;
-    DrawLineEx((Vector2){ topLeft.x, y }, (Vector2){ bottomRight.x, y }, thickness, col);
+    DrawLineEx((Vector2){topLeft.x, y}, (Vector2){bottomRight.x, y}, thickness,
+               col);
   }
 }
 #endif
